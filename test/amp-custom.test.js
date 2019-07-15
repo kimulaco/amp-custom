753b4c869b38fd0ec2a2d43ce5304cd127c080f8
kimulaco <kmrkdev@gmail.com>
2019-07-15 17:51:55 +0900

    Add eslint

--- test/amp-custom.test.js
+++ test/amp-custom.test.js
@@ -1,37 +1,36 @@
-const fs = require('fs');
-const path = require('path');
-const inputCss = fs.readFileSync(path.join(__dirname, './css/input.css')).toString();
+const fs = require('fs')
+const path = require('path')
+const inputCss = fs.readFileSync(path.join(__dirname, './css/input.css')).toString()
 
 describe('amp-custom', () => {
-    const AmpCustom = require('../amp-custom');
-    const ampCustom = new AmpCustom();
-    let output = '';
+  const AmpCustom = require('../amp-custom')
+  const ampCustom = new AmpCustom()
+  let output = ''
 
-    test('.optimize(cssSource)', () => {
-        output = ampCustom.optimize(inputCss);
+  test('.optimize(cssSource)', () => {
+    output = ampCustom.optimize(inputCss)
 
-        expect(typeof output).toBe('string');
-    });
+    expect(typeof output).toBe('string')
+  })
 
-    test('.getSize(cssSource)', () => {
-        let size = ampCustom.getSize(output);
+  test('.getSize(cssSource)', () => {
+    const size = ampCustom.getSize(output)
 
-        expect(typeof size).toBe('number');
-    });
+    expect(typeof size).toBe('number')
+  })
 
-    test('.isOverMaxByte(cssSource)', () => {
-        let bool = ampCustom.isOverMaxByte(output);
+  test('.isOverMaxByte(cssSource)', () => {
+    const bool = ampCustom.isOverMaxByte(output)
 
-        expect(bool).toBe(false);
-    });
+    expect(bool).toBe(false)
+  })
 });
 
 (() => {
-    const AmpCustom = require('amp-custom');
-    const ampCustom = new AmpCustom();
-    const outputPath = path.join(__dirname, './css/output.css');
-    const outputCss = ampCustom.optimize(inputCss);
+  const AmpCustom = require('amp-custom')
+  const ampCustom = new AmpCustom()
+  const outputPath = path.join(__dirname, './css/output.css')
+  const outputCss = ampCustom.optimize(inputCss)
 
-    fs.writeFile(outputPath, outputCss);
-})();
-
+  fs.writeFile(outputPath, outputCss)
+})()
